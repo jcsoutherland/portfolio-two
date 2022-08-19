0bcf880ed0a2d368c8517250fa1ba27ce8f0d6e6
Joey Southerland <31213840+jcsoutherland@users.noreply.github.com>
2022-08-19 14:17:21 -0500

    added work section template

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -5,10 +5,19 @@ const Work = () => {
   return (
     <div className='work-container'>
       <span className='Title-Text work-title'>WORK</span>
-      <WorkCard />
-      <WorkCard />
-      <WorkCard />
-      <WorkCard />
+      {workArray.map((e, index) => {
+        return (
+          <WorkCard
+            key={index}
+            video={e.video}
+            name={e.name}
+            placeholderimage={e.placeholderimage}
+            description={e.description}
+            codelink={e.codelink}
+            websitelink={e.websitelink}
+          />
+        )
+      })}
     </div>
   )
 }
@@ -16,14 +25,40 @@ const Work = () => {
 const WorkCard = (props) => {
   return (
     <div className='work-card-container'>
-      <span className='work-card-title'>{props.title}</span>
-      {/*Video and image if no load video*/}
-      <img
-        className='work-card-thumbnail-image'
-        src={props.placeholder_image}
-        alt={`${props.title} thumbnail image`}
-      />
-      <p className='work-card-description'>{props.description}</p>
+      <div className='work-name-container'>
+        <span className='work-card-name'>{props.name}</span>
+        {/*Video and image if no load video*/}
+        <img
+          className='work-card-thumbnail-image'
+          src={props.placeholderimage}
+          alt={`${props.name} thumbnail image`}
+        />
+      </div>
+      <div className='work-description-container'>
+        <p className='work-card-description'>{props.description}</p>
+        <div className='work-button-container'>
+          <div
+            className={`work-card-button ${
+              props.codelink === '' && 'button-disabled'
+            }`}
+            onClick={() => {
+              window.open(props.codelink, '_blank')
+            }}
+          >
+            CODE
+          </div>
+          <div
+            className={`work-card-button ${
+              props.websitelink === '' && 'button-disabled'
+            }`}
+            onClick={() => {
+              window.open(props.websitelink, '_blank')
+            }}
+          >
+            WEBSITE
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Data/workArray.js
+++ src/Data/workArray.js
@@ -2,7 +2,7 @@ const workArray = [
   {
     name: 'Platform Skater',
     video: '',
-    placeholderimage: '',
+    placeholderimage: '../Media/caveguythumbnail.png',
     description:
       'Platform Skater is a Game Boy Advance game where the player must navigate through on-coming obstacles as long as possible. This game was built entirely in C using a GBA emulator.',
     codelink: 'https://github.com/jcsoutherland/GBAPlatform-Skater',
@@ -11,7 +11,7 @@ const workArray = [
   {
     name: 'Pictar.io',
     video: '',
-    placeholderimage: '',
+    placeholderimage: '../Media/caveguythumbnail.png',
     description:
       'Developed alongside a group of friends, I was the sole graphic designer as well as the primary game designer. Pictar.io is an online pictionary game where you can play various pictionary game modes and style your avatar to be true to you! While working on this project I used various design tools such as Adobe Illustrator, Photoshop, and Figma.',
     codelink: '',
@@ -20,7 +20,7 @@ const workArray = [
   {
     name: 'Faux Admin Panel',
     video: '',
-    placeholderimage: '',
+    placeholderimage: '../Media/caveguythumbnail.png',
     description:
       'In this project I designed a desktop application for a faux movie theater chain that allows staff of the theaters to request information from within their companies databases, such as: current movies, ticket sales, movie ratings, and theater locations. This project was developed in C# using Dapper to send query requests to the Microsoft SQL Server database.',
     codelink: 'https://github.com/jcsoutherland/AdminDatabasePanel',
@@ -29,7 +29,7 @@ const workArray = [
   {
     name: 'Cave Guy',
     video: '',
-    placeholderimage: '../../public/Media/caveguythumbnail.png',
+    placeholderimage: '../Media/caveguythumbnail.png',
     description:
       'Cave Guy is a dungeon boss fighter game, designed to be a speedrunning game with difficult boss mechanics set to be mastered by the best. This game was built using C# and Microsoft XNA.',
     codelink: 'https://github.com/jcsoutherland/Cave-Guy',
